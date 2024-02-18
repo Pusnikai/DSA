f415036c69bafc4c969b8e59efedd952840a0e70
Prashanth Nallathamby <prashanth.baskaran101@gmail.com>
2024-02-18 12:45:27 -0500

    I'm Done with this

--- INFR-2820U_1.py
+++ INFR-2820U_1.py
@@ -1,8 +1,11 @@
-#INFR-2080U Assignment 1
-#Prashanth B.Nallathamby 100784991
-
+"""
+INFR-2080U Assignment 1
+Prashanth B.Nallathamby 100784991
+"""
 import time
 
+#Data Structure
+#Data Structure to store the information of each Products
 def Initial_product_data():
     products = []
     filename = "product_data.txt"
@@ -12,12 +15,14 @@ def Initial_product_data():
             products.append((int(productFile[0]), productFile[1], float(productFile[2]), productFile[3]))
     return products
 
+#Function to display all the available products
 def Show_Products(products):
     strProd = ""
     for product in products:
         strProd += "ID:" + str(product[0]) + " Name:" + product[1] + " Price:" + str(product[2]) + " Category:" + product[3] + "\n"
     return strProd
 
+#Functions that search for the products based on ID or Name
 def Search_Product(idORname, products):
     for i, product in enumerate(products):
         if isinstance(idORname, int) and product[0] == idORname:
@@ -26,6 +31,7 @@ def Search_Product(idORname, products):
             return f"ID:{product[0]} Name:{product[1]} Price:{product[2]} Category:{product[3]}\n"
     return None
 
+#Function that insert new product into the array of products
 def Insert_Product(products, id, name, price, category):
     Condition = True
     for product in products:
@@ -38,20 +44,24 @@ def Insert_Product(products, id, name, price, category):
         print("Product Succesfully  Added\n")
     return  products
 
+#Function that updates the choosen product's attribute
 def Update_Product(products, id, name, price, category):
     Location = int(Search_Product(id,products))
     products[Location] = (id,name,price,category)
     print("Products Updated\n")
     return products
 
+#Function that removes a chosen product from the list
 def Delete_Product(id, products):
     Location = int(Search_Product(id,products))
     products.pop(Location)
     print("Product Deleted\n")
     return products
 
+#Sorting Algorithm (Bubble sort)
+#Sorts based on ascending or descending
 def Sort_Product(products, Order):
-    start = time.time()
+    start = time.time() #Start time of sorting
     for i in range(len(products)):
         for  j in range(0, len(products)-i-1):
             if Order == "Ascending":
@@ -60,9 +70,11 @@ def Sort_Product(products, Order):
             if Order == "Descending":
                 if products[j][2] < products[j+1][2]:
                     products[j], products[j+1] = products[j+1], products[j]
-    end = time.time()
-    print("Sorting Time (seconds): ",end-start)
+    end = time.time() #End time of Sorting
+    print("Sorting Time (seconds): ",end-start) #Print the time taken to sort for time complexity
     return products  
+
+#Main Function that uses while loop for user interaction
 def main():
     products = Initial_product_data()
     while True:
